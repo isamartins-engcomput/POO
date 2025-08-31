0e9b807523c4e92c375053a13fb8f7495179bf05
isamartins-engcomput <isadoramartins1906@gmail.com>
2025-08-31 12:50:49 -0300

    Primeiro commit

---
+++ aula2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std; // ????
+// Instaciação ????
+
+/* int main()
+{
+  int i = 02;
+  cout << "AULA n POO " << i;
+
+  return 0;
+} */
+
+class Mouse
+{
+  private:
+    // Atributos -> Características
+    int sn;
+    bool sem_fio;
+    string marca;
+    string modelo;
+    int dpi;
+
+  public:
+    // Construtor -> Quase como uma função, mas diferente por conta do escopo
+    Mouse(int sn, bool sem_fio, string marca, string modelo, int dpi)
+    {
+      // this->atributo = variável;
+      this->sn = sn;
+      this->sem_fio = sem_fio;
+      this-> marca = marca;
+      this-> modelo = modelo;
+      this-> dpi = dpi;
+    }
+};
